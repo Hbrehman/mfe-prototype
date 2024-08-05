c1c2625b5bc6d7b23c1126c052258e00aca2b3a7
hbrehman <habib.rehman02@arbisoft.com>
2024-08-05 10:29:18 +0500

    randomize production classnames

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -19,9 +19,6 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap'
             },
             shared: packageJson.dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
         })
     ]
 }
