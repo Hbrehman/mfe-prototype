8433444f8cbe248515fafb93197d2b6f88b5611f
hbrehman <habib.rehman02@arbisoft.com>
2024-08-05 18:56:56 +0500

    implement communication between container and remote apps

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,6 +1,11 @@
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import React from 'react'
-import { Switch, Route, BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
+// BrowserRouter internally creates a copy of browser history 
+// Router: simple Router allows us to provide history object we wanted to use rather then react-router-dom 
+// create history object for us. Router does not create its own history which means we have to provide history to use
+// 
+import { Switch, Route, Router } from 'react-router-dom/cjs/react-router-dom.min';
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
@@ -8,15 +13,15 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'ma'
 })
 
-export default function App() {
+export default function App({ history }) {
   return (
     <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+      <Router history={history}>
         <Switch>
             <Route exact path='/pricing' component={Pricing}/>
             <Route exact path='/' component={Landing} />
         </Switch>
-          </BrowserRouter>
+      </Router>
     </StylesProvider>
   )
 }
